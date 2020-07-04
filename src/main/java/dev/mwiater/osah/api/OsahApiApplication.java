37179560c1d06703de6f3d6580e3ee359ef4042e
Mateusz Wiater <mateuszwiater@gmail.com>
2020-07-04 01:37:51 -0500

    Initial commit.

---
+++ src/main/java/dev/mwiater/osah/api/OsahApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mwiater.osah.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsahApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsahApiApplication.class, args);
+	}
+
+}
